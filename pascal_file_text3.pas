219713adf3129728ac46c45a9796ff80b9a3dcd8
Kr0nys <144237820+Kr0nys@users.noreply.github.com>
2023-12-14 16:28:42 +0300

    Add files via upload

---
+++ pascal_file_text3.pas
@@ -0,0 +1,12 @@
+﻿Program pascal_file_text3;
+var
+   filetext : text;
+   s : string;
+begin
+assign(filetext,'text.txt');
+append(filetext);
+writeln('Введите строку: ');
+readln(s);
+write(filetext, s);
+close(filetext);
+end.
